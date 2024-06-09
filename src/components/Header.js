88b336f8c6c7640ee19ccd33f4c7904863e39c41
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-06-09 17:14:52 -0300

    feat(app): add functionality that allows users to sign in and synchronize with crypto wallet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,26 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import { doLogin } from '@/services/Web3Service';
+
 export function Header() {
+  const [wallet, setWallet] = useState('');
+
+  useEffect(() => {
+    setWallet(localStorage.getItem('wallet') || '');
+  }, []);
+
+  function handleSignIn() {
+    doLogin()
+      .then((wallet) => setWallet(wallet))
+      .catch((error) => {
+        console.error(error);
+
+        alert(error.message);
+      });
+  }
+
   return (
     <header className="p-3 text-bg-dark">
       <div className="container">
@@ -11,13 +33,20 @@ export function Header() {
             <h1 className="fw-bold text-light">FloodHelp</h1>
           </a>
           <div className="text-end col-9">
-            <button type="button" className="btn btn-outline-light me-2">
-              <img src="/metamask.svg" width="24" className="me-3" />
-              Sign In
-            </button>
-            <a href="/create" className="btn btn-warning">
-              Ask for help
-            </a>
+            {wallet ? (
+              <a href="/create" className="btn btn-warning">
+                Ask for help
+              </a>
+            ) : (
+              <button
+                type="button"
+                className="btn btn-outline-light me-2"
+                onClick={handleSignIn}
+              >
+                <img src="/metamask.svg" width="24" className="me-3" />
+                Sign In
+              </button>
+            )}
           </div>
         </div>
       </div>
---
+++ src/services/Web3Service.js
@@ -0,0 +1,16 @@
+import Web3 from 'web3';
+
+export async function doLogin() {
+  if (!window.ethereum)
+    throw new Error('Metamask ethereum wallet is not available!');
+
+  const web3 = new Web3(window.ethereum);
+
+  const accounts = await web3.eth.requestAccounts();
+
+  if (!accounts || !accounts.length) throw new Error('Wallet not allowed!');
+
+  localStorage.setItem('wallet', accounts[0].toLowerCase());
+
+  return accounts[0];
+}
