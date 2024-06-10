c430d63c7ed9faae96f34a18ca3eb039221256ce
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-06-09 21:28:37 -0300

    chore: create function to get open requests in contract address retrieved through application binary interface

--- src/services/Web3Service.js
+++ src/services/Web3Service.js
@@ -1,5 +1,9 @@
 import Web3 from 'web3';
 
+import ABI from './ABI.json';
+
+const CONTRACT_ADDRESS = '0xdA440067cFBA0Bc0cb36AcAc090E4De385135cdf';
+
 export async function doLogin() {
   if (!window.ethereum)
     throw new Error('Metamask ethereum wallet is not available!');
@@ -13,4 +17,23 @@ export async function doLogin() {
   localStorage.setItem('wallet', accounts[0].toLowerCase());
 
   return accounts[0];
+}
+
+export async function getOpenRequests(lastId = 0) {
+  if (!window.ethereum)
+    throw new Error('Metamask ethereum wallet is not available!');
+
+  const authenticatedUserWallet = localStorage.getItem('wallet');
+
+  const web3 = new Web3(window.ethereum);
+
+  const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS, {
+    authenticatedUserWallet,
+  });
+
+  const requests = await contract.methods
+    .getOpenRequests(lastId + 1, 10)
+    .call();
+
+  return requests.filter((request) => request.title !== '');
 }
